58fafd7f658edf286504cf9e1e5885c53ce8b60e
ryanfigo <ryanfigo12@gmail.com>
2021-10-06 16:23:20 +0800

    tugas saya

---
+++ frontpage/beranda.php
@@ -0,0 +1,123 @@
+<div class="flex flex-col w-full item-center lg:items-start overflow-y-hidden">
+			<h1 class="my-4 text-3xl md:text-5xl text-black-100 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Converse Shoes</h1>
+			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Menyediakan Segala Jenis Shoes</p>
+		</div>
+		
+		<!--Right Col-->
+		<div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
+            <img class="w-5/6 mx-auto lg:mr-0 slide-in-bottom" src="asssets/LOGO1.jpg"> 
+		</div>
+
+        <div class="grid grid-cols-3 gap-2">
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/converse1.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Converse Chuck 70</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">Gaya lo pasti bakal makin keren dengan Converse Chuck 70 ini. Kanvas premium kuat, dilengkapi bantalan ringan yang bakalan bikin lo nyaman beraktivitas.</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/converse2.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Converse Hybrid Camo Chuck 70 </div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">Desain camo yang dicetak full-print dengan sablon dan dilapisi suede di atas kanvas organik khas Chuck 70.</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/converse3.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Chuck 70 High </div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">Lebih banyak bantalan dan kanvas yang lebih kuat. Chuck 70 High Top dibuat dari desain asli tahun 1970,</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/adidas1.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Adidas Ultraboost Marimekko</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">adidas Ultraboost Marimekko hadir dengan motif yang menampilkan keceriaan. Dengan desainnya yang menarik sepatu ini memberikan nuansa baru saat berlari.
+</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/adidas2.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Adidas UltraBOOST ‘White Tuxedo’</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">Inspired by Bond’s white tuxedo, this is a triple white shoe fit for the red carpet.</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/adidas3.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">adidas x Karlie Kloss</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">adidas women adalah komunitas untuk semua orang. Mari saling mendukung, menginspirasi, dan belajar dari satu sama lain. </p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/vans1.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Anaheim Factory</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">Anaheim Factory kembali mengeluarkan koleksi Anaheim Factory Needlepoint Pack yang populer dan memberikan nuansa era 70-80an.</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/vans2.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Vans dan Pendleton</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">ans dan Pendleton bergabung untuk menciptakan alas kaki yang khas dengan memadukan warna-warna cerah dengan pola unik Pendleton pada sepatu klasik Vans. The Anaheim Factory Pendleton Pack, available at Vans stores!</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/vans3.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Vans Korean Typography!</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500"> Telah hadir dalam Old Skool dan Slip-On dengan detail ukiran bahasa Korea pada sisisnya. Available at Vans stores! </p>
+				</div>
+				</div>
+			</div>
+		</div>
+
+		</div>
---
+++ frontpage/rekap.php
@@ -0,0 +1,77 @@
+<div class="items-center bg-white rounded-xl shadow-md overflow-hidden md:max-w-full slide-in-bottom">
+        <div class="py-12">
+          <h2 class="grid grid-cols-4 gap-4 p-2 text-2xl font-bold">Pembelian</h2>
+            <div class="mt-8 max-w-md">
+            <div class="grid grid-cols-1 gap-6">
+              <label class="block">
+                <span class="text-gray-700">Nama Lengkap</span>
+                <input type="text" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" placeholder=""/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">No HP</span>
+                <input type="number" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" placeholder=""/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Alamat</span>
+                <input type="text" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Tanggal Pemesanan</span>
+                <input type="Date" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Merk Sepatu</span>
+                <select class="block w-full mt-1  rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0">
+                <option></option>   
+                <option>Converse</option>
+                  <option>Adidas</option>
+                  <option>Vans</option>
+                </select>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Ukuran Sepatu</span>
+                <select class="block w-full mt-1  rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0">
+                <option></option>  
+                <option>37</option>
+                  <option>38</option>
+                  <option>39</option>
+                  <option>40</option>
+                  <option>42</option>
+                  </select>
+
+                </label>
+                  <label class="block">
+                <span class="text-gray-700">Jenis Sepatu Yang Dipilih</span>
+                <select class="block w-full mt-1  rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0">
+                <option></option>  
+                <option>CONVERSE CHUCK 70</option>
+                  <option>CONVERSE HYBRID CAMO CHUCK 70</option>
+                  <option>CHUCK 70 HIGH</option>
+                  <option>ADIDAS ULTRABOOST MARIMEKKO</option>
+                  <option>ADIDAS ULTRABOOST ‘WHITE TUXEDO’</option>
+                  <option>ADIDAS X KARLIE KLOSS</option>
+                  <option>ANAHEIM FACTORY</option>
+                  <option>VANS DAN PENDLETON</option>
+                  <option>VANS KOREAN TYPOGRAPHY!</option>
+                  </select>
+              <label class="block">
+                <span class="text-gray-700">Metode Pembayaran</span>
+                <select class="block w-full mt-1  rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0">
+                <option></option>   
+                <option>BRI</option>
+                  <option>BNI</option>
+                  <option>BCA</option>
+                  <option>DANA</option>
+                  <option>COD (Cash On Delivery)</option>
+                </select>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Keterangan pada penjual</span>
+                <textarea
+                  class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" rows="3"></textarea>
+              </label>
+              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pesan</button></a>
+            </div>
+            </div>
+        </div>
+</div>
